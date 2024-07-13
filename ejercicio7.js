77aae2866f62313a6cc70954c6eae618c5ec8ed2
Migueks <lhlmiguel@gmail.com>
2024-07-13 11:30:14 +0200

    ejericio 7 finalizado

---
+++ ejercicio7.js
@@ -0,0 +1,11 @@
+function greaterNumber(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne);
+  } else if (numberTwo > numberOne) {
+    console.log(numberTwo);
+  } else {
+    console.log("Los número son iguales");
+  }
+}
+
+greaterNumber(4, 4);
