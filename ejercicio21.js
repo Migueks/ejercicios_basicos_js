71db862b6563e51b9089d7e9e0f0f5d01e57aedd
Migueks <lhlmiguel@gmail.com>
2024-07-22 15:43:01 +0200

    ejercicio 21 finalizado

---
+++ ejercicio21.js
@@ -0,0 +1,21 @@
+const users = [
+  { name: "Tony", years: 43 },
+  { name: "Peter", years: 18 },
+  { name: "Natasha", years: 14 },
+  { name: "Bruce", years: 32 },
+  { name: "Khamala", years: 16 },
+];
+
+menores = [];
+mayores = [];
+
+for (const user of users) {
+  if (user.years < 18) {
+    menores.push(" " + user.name);
+  } else {
+    mayores.push(" " + user.name);
+  }
+}
+
+console.log(`Usuarios menores de edad:${menores}`);
+console.log(`Usuarios mayorea de edad:${mayores}`);
