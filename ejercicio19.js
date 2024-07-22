996a41d8a98fe9000ba0e049ec4494d5a9b2d7aa
Migueks <lhlmiguel@gmail.com>
2024-07-22 14:25:54 +0200

    ejercicio 19 finalizado

---
+++ ejercicio19.js
@@ -0,0 +1,17 @@
+const toys = [
+  { id: 5, name: "Transformers" },
+  { id: 11, name: "LEGO" },
+  { id: 23, name: "Hot Wheels" },
+  { id: 40, name: "Rascador de gato" },
+  { id: 40, name: "FurReal Friends gato interactivo" },
+  { id: 60, name: "Nerf Blaster" },
+  { id: 71, name: "Sylvanian Families - Familia gato" },
+];
+
+for (const key of toys) {
+  if (key.name.includes("gato")) {
+    toys.splice(key, 1);
+  } else {
+    console.log(key);
+  }
+}
